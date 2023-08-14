bdc303db6f2ef02e2edf27d71a7d5822e3a5bfcc
Saiful Islam <98213668+azadgmtl@users.noreply.github.com>
2023-08-14 16:17:08 +0600

    another changed

--- js/script.js
+++ js/script.js
@@ -10,12 +10,32 @@ const allahAkberDisplay = document.getElementById('allahAkberDisplay');
 const allahAkberIncrimentBtn = document.getElementById('allahAkberIncrimentBtn');
 const allahAkberDecrimentBtn = document.getElementById('allahAkberDecrimentBtn');
 
+
+const laIlahaDisplay = document.getElementById('laIlahaDisplay');
+const laIlahaIncrement = document.getElementById('laIlahaIncrement');
+const laIlahaDecrement = document.getElementById('laIlahaDecrement');
+
+
+const astagFirullahDisplay = document.getElementById('astagFirullahDisplay');
+const astagFirullahIncrement = document.getElementById('astagFirullahIncrement');
+const astagFirullahDecrement = document.getElementById('astagFirullahDecrement');
+
+
+const darudDisplay = document.getElementById('darudDisplay');
+const darudIncrement = document.getElementById('darudIncrement');
+const darudDecrement = document.getElementById('darudDecrement');
+
+
+
 const resetBtn = document.getElementById('resetBtn');
 
 
 let SubhanAllahinitValue = 0;
 let AlhamdulillahInitValue = 0;
 let AllahuAkbarInitValue = 0;
+let LaIlahaInitValue = 0;
+let AstagFirullahInitValue = 0;
+let DarudInitValue = 0;
 
 
 /* ======= সুবহানাল্লাহ Increment and Decrement ===== */
@@ -54,7 +74,7 @@ alhamdulillahIncrimentBtn.addEventListener('click', function(){
 
 alhamdulillahDecrimentBtn.addEventListener('click', function(){
     if(AlhamdulillahInitValue == 0){
-        return alert("'আলহামদুলিল্লাহ' task completed");
+        return alert("Sorry, You can't add negative value");
     }
 
     AlhamdulillahInitValue -= 1;
@@ -78,14 +98,78 @@ allahAkberIncrimentBtn.addEventListener('click', function(){
 
 allahAkberDecrimentBtn.addEventListener('click', function(){
     if(AllahuAkbarInitValue == 0){
-        return alert("'আলহামদুলিল্লাহ' task completed");
+        return alert("Sorry, You can't add negative value");
     }
 
     AllahuAkbarInitValue -= 1;
     allahAkberDisplay.innerText = AllahuAkbarInitValue;
 });
 
-/* ======= আল্লাহু আকবর  Increment and Decrement ===== */
+
+
+
+
+/* ======= লা ইলাহা ইল্লাহ  Increment and Decrement ===== */
+laIlahaIncrement.addEventListener('click', function(){
+    if(LaIlahaInitValue == 33){
+        return alert("'লা ইলাহা ইল্লাহ ' task completed");
+    }
+
+    LaIlahaInitValue += 1;
+    laIlahaDisplay.innerText = LaIlahaInitValue;
+});
+
+laIlahaDecrement.addEventListener('click', function(){
+    if(LaIlahaInitValue == 0){
+        return alert("Sorry, You can't add negative value");
+    }
+
+    LaIlahaInitValue -= 1;
+    laIlahaDisplay.innerText = LaIlahaInitValue;
+});
+
+
+
+
+/* ======= আস্তাগফিরুল্লাহ  Increment and Decrement ===== */
+astagFirullahIncrement.addEventListener('click', function(){
+    if(AstagFirullahInitValue == 33){
+        return alert("'আস্তাগফিরুল্লাহ ' task completed");
+    }
+
+    AstagFirullahInitValue += 1;
+    astagFirullahDisplay.innerText = AstagFirullahInitValue;
+});
+
+astagFirullahDecrement.addEventListener('click', function(){
+    if(AstagFirullahInitValue == 0){
+        return alert("Sorry, You can't add negative value");
+    }
+
+    AstagFirullahInitValue -= 1;
+    astagFirullahDisplay.innerText = AstagFirullahInitValue;
+});
+
+
+
+/* ======= সাল্লাল্লাহু আলাইহী ওয়াসাল্লাম  Increment and Decrement ===== */
+darudIncrement.addEventListener('click', function(){
+    if(DarudInitValue == 33){
+        return alert("'সাল্লাল্লাহু আলাইহী ওয়াসাল্লাম ' task completed");
+    }
+
+    DarudInitValue += 1;
+    darudDisplay.innerText = DarudInitValue;
+});
+
+darudDecrement.addEventListener('click', function(){
+    if(DarudInitValue == 0){
+        return alert("Sorry, You can't add negative value");
+    }
+
+    DarudInitValue -= 1;
+    darudDisplay.innerText = DarudInitValue;
+});
 
 
 
@@ -95,8 +179,15 @@ resetBtn.addEventListener('click', function(){
     subhanAllahDisplay.innerText = 0;
     alhamdulillahDisplay.innerText = 0;
     allahAkberDisplay.innerText = 0;
+    laIlahaDisplay.innerText = 0;
+    astagFirullahDisplay.innerText = 0;
+    darudDisplay.innerText = 0;
+
 
     SubhanAllahinitValue = 0;
     AlhamdulillahInitValue = 0;
     AllahuAkbarInitValue = 0;
+    LaIlahaInitValue = 0;
+    AstagFirullahInitValue = 0;
+    DarudInitValue = 0;
 })
